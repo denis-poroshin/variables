d5c9160eacf717bdbf9e25960c06d8aa03bfc919
original <git@github.com:denis-poroshin/variables.git>
2024-01-12 21:38:14 +0300

    homework done №2

---
+++ src/Main.java
@@ -0,0 +1,97 @@
+public class Main {
+    public static void main(String[] args) {
+        // Буду прописывать номера заданий для обеспечения читабельности кода (надеюсь это не будет ошибкой =))
+        // Задание 1
+        System.out.println("Задание 1");
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        // Задание 2
+        System.out.println("Задание 2");
+        var dogSum = dog + 2;
+        var catSum = cat + 2;
+        var paperSum = paper + 2;
+        System.out.println(dogSum);
+        System.out.println(catSum);
+        System.out.println(paperSum);
+        // Задание 3
+        System.out.println("Задание 3");
+        var dogMinus = dog - 3.5;
+        var catMinus  = cat - 1.6;
+        var paperMinus  = paper - 7639;
+        System.out.println(dogMinus);
+        System.out.println(catMinus);
+        System.out.println(paperMinus);
+        // Задание 4
+        System.out.println("Задание 4");
+        var friend = 19;
+        System.out.println(friend);
+        var friendMultiplication = friend * 2;
+        System.out.println(friendMultiplication);
+        var  frienddDvision = friendMultiplication / 7;
+        System.out.println(frienddDvision);
+        // Задание 5
+        System.out.println("Задание 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        var frogMultiplication = frog * 10;
+        System.out.println(frogMultiplication);
+        var frogDvision = frogMultiplication / frog;
+        System.out.println(frogDvision);
+        var frogSum = frogDvision + 4;
+        System.out.println(frogSum);
+        // Задание 6
+        System.out.println("Задание 6");
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var boxerSum = boxer2 + boxer1;
+        var boxerDifference = boxer2 - boxer1;
+        System.out.println("Общая масса двух бойцов " + boxerSum);
+        System.out.println("Разница между массами бойцов " + boxerDifference);
+        // Задание 7
+        System.out.println("Задание 7");
+        var boxerNew = 78.2;
+        var boxerNew1 = 82.7;
+        var boxerDifferenceNew = boxerNew1 - boxerNew;
+        var boxerDifferenceNew1 = boxerNew1 % boxerNew;
+        System.out.println("Разница между массами бойцов " + boxerDifferenceNew);
+        System.out.println("Разница между массами бойцов " + boxerDifferenceNew1);
+        // Задание 8
+        System.out.println("Задание 8");
+        var hour = 640;
+        var humanHour = 8;
+        var human = hour / humanHour;
+        System.out.println("Всего работников в компании — " + human + " человек");
+        var humanAdded = 94;
+        var humanSum = human + humanAdded;
+        var humanHourNew = humanSum * humanHour;
+        System.out.println("Если в компании работает " + humanSum + " человек, то всего " + humanHourNew +
+                " часов работы может быть поделено между сотрудниками");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
